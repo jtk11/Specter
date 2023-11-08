792027baa3f5776a6ddd0c73ac74baeceb1e18d6
jtk11 <149712366+jtk11@users.noreply.github.com>
2023-11-08 11:33:28 -0500

    reverb added but not implemented

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -8,6 +8,7 @@
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include "Reverb.h"
 
 //==============================================================================
 SpecterAudioProcessor::SpecterAudioProcessor()
---
+++ Source/Reverb.h
@@ -0,0 +1,52 @@
+/*
+  ==============================================================================
+
+    Reverb.h
+    Created: 8 Nov 2023 11:25:58am
+    Author:  MacBook Pro
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+class ReverbEffect
+{
+public:
+    ReverbEffect() {}
+    ~ReverbEffect() {}
+
+    void prepare(const juce::dsp::ProcessSpec& spec)
+    {
+        reverb.prepare(spec);
+    }
+
+    void reset()
+    {
+        reverb.reset();
+    }
+
+    void process(juce::AudioBuffer<float>& buffer)
+    {
+        auto block = juce::dsp::AudioBlock<float>(buffer);
+        auto context = juce::dsp::ProcessContextReplacing<float>(block);
+        reverb.process(context);
+    }
+
+    void updateParameters(float roomSize, float damping, float wetLevel, float dryLevel, float width, float freezeMode)
+    {
+        juce::dsp::Reverb::Parameters params;
+        params.roomSize = roomSize;
+        params.damping = damping;
+        params.wetLevel = wetLevel;
+        params.dryLevel = dryLevel;
+        params.width = width;
+        params.freezeMode = freezeMode;
+        reverb.setParameters(params);
+    }
+
+private:
+    juce::dsp::Reverb reverb;
+};
